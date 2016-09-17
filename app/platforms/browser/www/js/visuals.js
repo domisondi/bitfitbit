a73dd5f689dd332b8ef859a1bb89139b978469b1
Robin Bisping <robin@bisping.ch>
2016-09-17 19:50:45 +0200

    Style

--- app/platforms/browser/www/js/visuals.js
+++ app/platforms/browser/www/js/visuals.js
@@ -11,7 +11,7 @@ $(function() {
 $('a.collection').height(.5 * $('a.collection').width());
 
 var colors = ['#00a0b0', '#cc333f', '#eb6841', '#edc951'];
-$('a.collection').each(function(i) {
+$('#collections a').each(function(i) {
 	$(this).prepend('<span>#' + (i+1).toString() + '</span>');
 	$(this).css('background-color', colors[i % colors.length]);
 });
